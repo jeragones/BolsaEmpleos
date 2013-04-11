b9c79c516dc8573331fc80f5f2bdccfc9fedabcd
leonardo castro <leock123@hotmail.com>
2013-04-11 11:50:52 -0600

    funcIncertarEmpleo

---
+++ CarteraEmpleo/Clases/cEmpleosDatos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarteraEmpleo.Clases
+{
+    public class cEmpleosDatos
+    {
+        cDBService webService = new cDBService();
+        public string insertar(string p_IdCatEmpleo,string p_Descripcion) {
+            int _iId_CatEmpleo;
+            if (p_Descripcion == "" | p_IdCatEmpleo == ""){
+                try {
+                    _iId_CatEmpleo = Convert.ToInt32(p_IdCatEmpleo);
+                }
+                catch (Exception e) { }
+            }
+            //webService.insertar_Empleos;
+            return "";
+        }
+    }
+}
