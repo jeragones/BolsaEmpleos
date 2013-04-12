27822f07a316eb1cab581d00f7ef763b6fd59d78
Jorge Rojas Aragonés <jeragones@gmail.com>
2013-04-11 23:54:31 -0600

    Script Persona

--- CarteraEmpleo/Scripts/jQueryPersona.js
+++ CarteraEmpleo/Scripts/jQueryPersona.js
@@ -1,1 +1,13 @@
-﻿
+﻿function Idiomas(lista) {
+    var list = lista.split(",");
+    var code = '';
+    for (var i = 0; i < list.length; i++) {
+        code += '<div id="idioma' + i + '" runat="server">' +
+                '<asp:Label runat="server" class="lblRegistrar" Text="' + list[i] + '" />' +
+                '' +
+                '</div>'
+    }
+
+    var idioma = '<div class="UsuarioLink"><a href="PerfilPersona.aspx">' + usuario + '</a></div>';
+    document.getElementById("Login").innerHTML = user;
+}
